2554998c3a39ac2865c949387df55a0ab91355bc
RlVlZ <blanchet.remi@proton.me>
2023-07-11 14:39:13 +0200

    SRecFile and SRecFileHandler + functions/classes for command pattern

--- srec_bytearray.py
+++ srec_bytearray.py
@@ -1,10 +1,18 @@
+import string
+
 from binascii import hexlify
 from collections import namedtuple
 from bisect import bisect_right
 
 from timeit import default_timer as timer
 
+WORD_LEN = 4    # word len in bytes
+
 Coord = namedtuple('Coord', ['srec_idx', 'data_idx'])
+
+#===============#
+# SRecors class #
+#===============#
 
 class SRecError(Exception):
     pass
@@ -58,6 +66,7 @@ class SRec():
     
     def __setitem__(self, position: int, value: int):
         self.data[position] = value
+        self.update_cks()
     
     def compute_cks(self):
         _cks = sum(self.count + self.address + self.data)
@@ -92,6 +101,10 @@ class SRec():
         _ret_str += hexlify(self.cks).decode('utf-8')
         return _ret_str.upper() 
 
+#======================#
+# SRecord sector class #
+#======================#
+
 class SRecSectorExcept(Exception):
     pass
 
@@ -103,15 +116,22 @@ class SRecSector:
         self.srecs= []
         self.bytes = bytearray()
     
+
+    def get_start_addr(self) -> int:
+        return self.start_address
+
+
     def continuous(self, address: int):
         return address == self.start_address + self.size
     
+
     def __contains__(self, obj):
         if type(obj) == int:
             return self.start_address <= obj <= self.start_address + self.size 
         elif type(obj) == SRec:
             return self.start_address <= obj.get_int_addr() <= self.start_address + self.size  
     
+
     def add_srec(self, srec: SRec):
         int_addr = srec.get_int_addr()
         if not self.continuous(int_addr):
@@ -131,10 +151,11 @@ class SRecSector:
             data_idx = position - self.addresses[srec_idx]
             return Coord(srec_idx=srec_idx, data_idx=data_idx)
     
-    def __getitem__(self, position):
+    def __getitem__(self, position: int):
         if position in self:
             return self.srecs[self.get_coord(position).srec_idx]
     
+
     def __setitem__(self, position, value: bytearray):
         if position not in self:
             raise SRecSectorExcept("given address is not in the sector")
@@ -149,20 +170,32 @@ class SRecSector:
                 position += 1
                 _coord = self.get_coord(position)
     
+
     def __iter__(self):
         return (srec for srec in self.srecs)
     
+
     def remap(self, new_start_address: int):
         _offset = self.start_address - new_start_address
         self.start_address = new_start_address
-        self.addresses = [address + _offset for address in self.addresses]
+        self.addresses = [address - _offset for address in self.addresses]
         for srec in self.srecs:
             # ? working ?
-            srec.address = bytearray.fromhex(hex(srec.get_int_addr() + _offset))
+            srec.address = bytearray.fromhex(hex(srec.get_int_addr() - _offset)[2:])
+            srec.update_cks()
     
-    def get_info(self) -> str:
-        return f"starts at address 0x{hexlify(self.srecs[0].address)}, finishes at address 0x{hexlify(self.srecs[-1].address)}"
 
+    def get_info(self) -> str:
+        _first_addr = hexlify(self.srecs[0].address).decode('utf-8')
+        _last_addr = hex(self.srecs[-1].get_int_addr() + self.srecs[-1].count[0])
+        return f"starts at address 0x{_first_addr}, finishes at address {_last_addr}"
+
+#====================#
+# SRecord file class #
+#====================#
+
+class SRecFileError(Exception):
+    pass
 
 class SRecFile:
     def __init__(self):
@@ -192,21 +225,160 @@ class SRecFile:
             srec = SRec.read_srec(string)
             if srec.type == '0':
                 self.headers.append(srec)
-            elif srec.type in ['7', '8', '9']:
+            elif srec.type in '789':
                 self.footers.append(srec)
             else:
                 self.add_data_srec(srec)
         end = timer()
         print(f"Importation of file took {end - start} seconds")
     
+    def __contains__(self, address):
+        for sector in self.sectors:
+            if address in sector:
+                return True
+        else:
+            return False
+
+    
     def sectors_infos(self):
         info_str = ''
         for i, sector in enumerate(self.sectors):
             info_str += f"Sector{i}: {sector.get_info()}\n"
         return info_str
+    
+
+    def patch(self, address, value):
+        for sector in self.sectors:
+            if address in sector:
+                sector[address] = value
+                break
+        else:
+            raise SRecFileError("address given not in file datas")
+    
+    def remap_sector(self, sector_idx, new_address):
+        self.sectors[sector_idx].remap(new_address)
+
+    def get_data(self, address: int, len: int) -> bytearray:
+        for sector in self.sectors:
+            if address in sector:
+                position = sector.start_address - address
+                return sector.bytes[position: position + len]
+        else:
+            raise SRecFileError("address given not in file datas")
 
 
+#========================#
+# SRecord Files Commands #
+#========================#
 
-srec_f = SRecFile()
-srec_f.read_file("test_file.s19")
-print(srec_f.sectors_infos())
+class SRecCmd:
+    def __init__(self, srec_f : SRecFile, cible, value):
+        self.srec_f = srec_f
+        self.cible = cible
+        self.value = value
+
+    def execute(self):
+        pass
+
+    def undo(self):
+        pass
+
+class SRecFPatchCmd(SRecCmd):
+    def __init__(self, srec_f: SRecFile, cible: int, value: bytearray):
+        super.__init__(srec_f, cible, value)
+        self.init_state = srec_f.get_data[cible, len(value)]
+    
+    def execute(self):
+        self.srec_f.patch(self.cible, self.value)
+
+    def undo(self):
+        self.srec_f.patch(self.cible, self.init_state)
+
+class SRecFRemapCmd(SRecCmd):
+    def __init__(self, srec_f: SRecFile, cible: int, value: int):
+        super.__init__(srec_f, cible, value)
+        self.old_address = srec_f.sectors[cible].get_start_addr()
+    
+    def execute(self):
+        self.srec_f.remap_sector(self.cible, self.value)
+    
+    def undo(self):
+        self.srec_f.remap_sector(self.cible, self.old_address)
+
+
+#===============================#
+# SRecord Files Command Invoker #
+#===============================#
+
+class SRecFileInvoker:
+    def __init__(self):
+        self.history = []
+
+    def execute(self, command: SRecCmd):
+        command.execute()
+        self.history.append(command)
+    
+    def undo_last(self):
+        self.history.pop().undo()
+
+
+#======================#
+# SRecord File Handler #
+#======================#
+
+class SRecFileHandlerError:
+    pass
+
+class SRecFileHandler:
+    def __init__(self, srec_f: SRecFile):
+        self.file_name = ''
+        self.file_path = ''
+        self.srec_f = srec_f
+        self.client = SRecFileInvoker()
+        self.scopes = []
+        self.tags = []
+    
+    def address_converter(self):
+        pass
+    
+    def value_converter(self):
+        pass
+
+    def add_scope(self):
+        pass
+
+    def add_tag(self):
+        pass
+
+    def display_scope(self):
+        pass
+
+    def hex_dump(self, address: int, addr_size, nb_words):
+
+        def space_frmt(bytes:str) -> str:
+            space_str = ' '.join([bytes[i:i+2] for i in range(0, len(bytes), 2)])
+            return space_str
+        
+        def get_word(data: bytearray) -> list[str]:
+            return [hexlify(data[i: i+WORD_LEN]).decode('utf-8') for i in range(0, len(data), WORD_LEN)]
+
+        if address in srec_f and (address + nb_words*WORD_LEN) in srec_f:
+            printables = set(string.printable) - set(string.whitespace)
+            addr_field = f"{{0:0>{addr_size}X}}".format(address)
+            data = self.srec_f.get_data(address, nb_words*WORD_LEN)
+            ascii_field = ''
+
+            for byte in data:
+                if chr(byte) in printables:
+                    ascii_field += chr(byte)
+                else:
+                    ascii_field += '.'
+
+            data_field = get_word(data)
+            data_field = [space_frmt(word) for word in data_field]
+
+            return addr_field + ':  ' + '   '.join(data_field).upper() + '  | ' + ascii_field + ' | '
+
+        else:
+            raise SRecFileHandlerError("Adress not in file")
+
